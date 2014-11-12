6d9aa74e052fc91e79101e0b8691c8c8ded1c2c7
David Sok <sokd88@gmail.com>
2014-11-11 21:27:29 -0500

    added foundation and video player

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,22 +16,4 @@
 //= require foundation
 //= require_tree .
 
-$(function(){
-  $(document).foundation();
-
-  $('.tabs-nav a').on('click', function(event) {
-    // Prevent the link from being followed (stay on same page)
-    event.preventDefault();
-
-    // Update current tab
-    var $currentTab = $(this);
-    $currentTab.addClass('active');
-    $currentTab.siblings().removeClass('active');
-
-    // Update current tab pane
-    var currentTabPaneSelector = $currentTab.attr('href');
-    var $currentTabPane = $(currentTabPaneSelector);
-    $currentTabPane.addClass('active');
-    $currentTabPane.siblings().removeClass('active');
-  });
-});
+$(document).foundation();
